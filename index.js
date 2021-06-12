2a4b882b3c623e847418ab99ea3b56a6d9b95cef
JS-practice <arti.neutroline@gmail.com>
2021-06-12 14:06:52 +0545

    added js file

---
+++ index.js
@@ -0,0 +1,9 @@
+//1. ways to print in javascript
+        //console.log("hello world");
+        //alert("me");
+       // document.write("this is a documnet write");
+
+      //2.console javascript API
+      console.log("Hello World");
+      console.warn("this is warning");
+      console.error("this is an error");
