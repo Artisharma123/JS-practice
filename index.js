5eb66ae1e6f7d13f61aa83f9523a53568bb3d123
JS-practice <arti.neutroline@gmail.com>
2021-06-14 09:17:58 +0545

    added var js file

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
        // document.write("this is a documnet write");
 
       //2.console javascript API
-      console.log("Hello World");
-      console.warn("this is warning");
-      console.error("this is an error");
+    //  console.log("Hello World");
+      //console.warn("this is warning");
+      //console.error("this is an error");
+
+      //3. javascript variable
+      //what is variable? - container to store the data value.
+      var number1 = 14;
+      var number2 = 12;
+      console.log("number1 + number2");
