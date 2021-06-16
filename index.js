2b6670e3c5b67c81d338ac2b29b440b3d0ef29a2
JS-practice <arti.neutroline@gmail.com>
2021-06-16 11:35:37 +0545

    added html file

--- index.js
+++ index.js
@@ -10,6 +10,40 @@
 
       //3. javascript variable
       //what is variable? - container to store the data value.
+
+      /*multi line command*/
+
       var number1 = 14;
       var number2 = 12;
-      console.log("number1 + number2");
+      console.log(number1 + number2);
+
+      //4. data type in js
+      //number
+      var num1 = 12;
+      var num2 = 20.43;
+
+      //string
+      var str1 = "this is a string";
+      var str2 = 'this is s also string';
+
+      //object
+      var marks ={
+        ravi: 87,
+        rani: 76,
+        aare: 99.98
+      }
+      console.log(marks);
+      console.log(str1);
+
+      //boolean
+      var a = true;
+      var b = false;
+      console.log(a,b);
+
+     // var und = undefined;
+     var und;
+      console.log(und);
+
+      //var n=null;
+      var n;
+      console.log(null);
